637d11b0ec93135edd6497f41a31196583954691
rukkya <rokiaossama@gmail.com>
2023-08-15 06:27:26 -0700

    ;

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include"main.h"
 int main(int ac , char **argv){
-char putput= "chill_shell$$";
-printf("%s",output);
+char output= "chill_shell$$";
+char *buffer_address;
+  size_t x=0;
+
+  printf("%s", output);
+  getline(&buffer_address, &x, stdin);
+
+  free(buffer_address);
 return (0);
 }
