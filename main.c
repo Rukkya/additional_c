7a1b285f7182264d347639cfb3a9c0661585a73c
rukkya <rokiaossama@gmail.com>
2023-08-15 06:54:12 -0700

    n

--- main.c
+++ main.c
@@ -1,14 +1,26 @@
 #include"main.h"
 int main(int ac , char **argv){
-char output= "chill_shell$$";
+char *output= "chillshell$$";
 char *buffer_address;
-  size_t x=0;
+ssize_t num;
+size_t x=0;
+  (void)ac , (void)argv;
 
+while(1){
   printf("%s", output);
   getline(&buffer_address, &x, stdin);
+num = getline(&buffer_address, &x, stdin);
+      /* check if the getline function failed or reached EOF or user use CTRL + D */ 
+        if (num == -1){
+            printf("Exiting shell....\n");
+            return (-1);
+        }
+
+
+
+
 printf("%s\n",buffer_address);
   free(buffer_address);
-
-  (void)ac; (void)argv;
+}
   return (0);
 }
